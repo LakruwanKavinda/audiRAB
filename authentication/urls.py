06e6e5544c127ff758dc61582cdeacc84684bc46
Lakruwan Kavinda <137679190+LakruwanKavinda@users.noreply.github.com>
2024-12-16 21:57:57 +0530

    Update urls.py
    new changes comment

--- authentication/urls.py
+++ authentication/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path('login/', views.login, name='login'), #url login
     path('upload_pdf/', views.upload_pdf, name='upload_pdf'), #url for pdf upload
     path('search_pdf/', views.search_pdf, name='search_pdf'), #url for search pdf
-    path('generate_story/', views.generate_story, name='generate_story'),
-    path('view_pdf/<int:pdf_id>/', views.view_pdf, name='view_pdf'),
-    # chat
+    path('generate_story/', views.generate_story, name='generate_story'), #generated story
+    path('view_pdf/<int:pdf_id>/', views.view_pdf, name='view_pdf'), # url for view the pdf
+    # chat part urls
     path('send_message/', views.send_message, name='send_message'),
     path('get_messages/', views.get_messages, name='get_messages'),
 ]
