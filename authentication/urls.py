4b3d938d3129b2af5870a4dc189fedef1df55b91
Lakruwan Kavinda <137679190+LakruwanKavinda@users.noreply.github.com>
2024-12-16 21:56:05 +0530

    Update urls.py
    comment

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,11 +6,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    # Change 'login' to 'user_login'
-    path('login/', views.login, name='login'),
-    path('upload_pdf/', views.upload_pdf, name='upload_pdf'),
-    path('search_pdf/', views.search_pdf, name='search_pdf'),
+    path('signup/', views.signup, name='signup'), #url signup
+    path('login/', views.login, name='login'), #url login
+    path('upload_pdf/', views.upload_pdf, name='upload_pdf'), #url for pdf upload
+    path('search_pdf/', views.search_pdf, name='search_pdf'), #url for search pdf
     path('generate_story/', views.generate_story, name='generate_story'),
     path('view_pdf/<int:pdf_id>/', views.view_pdf, name='view_pdf'),
     # chat
