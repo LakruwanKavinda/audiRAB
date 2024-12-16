d4cfc3a7e0bed0bb5d5c625ae2c8d0e71bc88069
Lakruwan Kavinda <137679190+LakruwanKavinda@users.noreply.github.com>
2024-12-16 23:05:25 +0530

    Update tests.py
    latest update

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from audiobooks.models import Audiobook
+from datetime import timedelta
 
-# Create your tests here.
+class AudiobookModelTestCase(TestCase):
+
+    def setUp(self):
+        # Set up data for test
+        self.audiobook = Audiobook.objects.create(
+            title="Sample Audiobook",
+            author="John Doe",
+            narrator="Jane Smith",
+            duration=timedelta(hours=1, minutes=30),
+            audio_file="test_audio.mp3",
+        )
+
+
+
