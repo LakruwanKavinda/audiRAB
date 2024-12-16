c88fbe4e311c4b8758b60496cf710c94229fb2f3
Lakruwan Kavinda <137679190+LakruwanKavinda@users.noreply.github.com>
2024-12-17 01:22:28 +0530

    Update models.py
    new latest

--- authentication/models.py
+++ authentication/models.py
@@ -45,6 +45,9 @@ class User(AbstractBaseUser):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=30)
     password = models.CharField(max_length=10)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
